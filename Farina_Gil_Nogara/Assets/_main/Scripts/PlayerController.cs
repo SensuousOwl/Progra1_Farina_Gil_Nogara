15ef3da0ac152a762dd9931bb62857aec43834d1
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-05-05 01:00:54 -0300

    Agregue las animaciones de Idle y Walking. Edite el animator para las transiciones y las active por codigo. Tambien arme una carpeta "Animations" para ir guardando las animaciones.

--- Farina_Gil_Nogara/Assets/_main/Scripts/PlayerController.cs
+++ Farina_Gil_Nogara/Assets/_main/Scripts/PlayerController.cs
@@ -6,8 +6,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 10;
     [SerializeField] private float zRotation;
 
+    private Animator myAnimator;
+
     private void Awake()
     {
+        myAnimator = gameObject.GetComponent<Animator>();
+
+        if(myAnimator == null)
+        {
+            Debug.LogError("Missing Animator Component.");
+        }
+
 
     }
 
@@ -15,26 +24,35 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,270);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,90);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W))
         {
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,180);
             transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else
+        {
+            myAnimator.SetBool("IsWalking", false);
         }
     }
 }
