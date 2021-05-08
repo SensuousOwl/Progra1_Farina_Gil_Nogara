b931ac9d52fe36d61d81dd28bbbded8d15a4cabc
SensuousOwl <lfarina888@gmail.com>
2021-05-07 21:03:33 -0300

    Funcionan las Animaciones!!!
    ＼(＾O＾)／

--- Farina_Gil_Nogara/Assets/_main/Scripts/PlayerController.cs
+++ Farina_Gil_Nogara/Assets/_main/Scripts/PlayerController.cs
@@ -6,9 +6,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 10;
     [SerializeField] private float zRotation;
 
-    //private Animator myAnimator;
+    private Animator myAnimator;
 
-    /*private void Awake()
+    private void Awake()
     {
         myAnimator = gameObject.GetComponent<Animator>();
 
@@ -18,41 +18,41 @@ public class PlayerController : MonoBehaviour
         }
 
 
-    }*/
+    }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            //myAnimator.SetBool("IsWalking", true);
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,270);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
-            //myAnimator.SetBool("IsWalking", true);
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,90);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
         else if (Input.GetKey(KeyCode.W))
         {
-            //myAnimator.SetBool("IsWalking", true);
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
-            //myAnimator.SetBool("IsWalking", true);
+            myAnimator.SetBool("IsWalking", true);
             transform.rotation = Quaternion.Euler(0,0,180);
             transform.position += transform.up * speed * Time.deltaTime;
         }
 
-        /*else
+        else
         {
             myAnimator.SetBool("IsWalking", false);
-        }*/
+        }
     }
 }
