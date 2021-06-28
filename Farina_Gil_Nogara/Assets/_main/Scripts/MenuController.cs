405c54a4e8688ab34701ce22ee28276aae220d4e
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-06-28 16:07:31 -0300

    We Have A Menu

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button exitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(OnClickPlay);
+        optionsButton.onClick.AddListener(OnClickOptions);
+        exitButton.onClick.AddListener(OnClickExit);
+    }
+
+    private void OnClickPlay()
+    {
+        SceneManager.LoadScene("LVL 1 Robot Arena");
+    }
+    private void OnClickOptions()
+    {
+
+    }
+    private void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
