1b9e36a1d60bdb23080f7ea3f906466141225b33
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-06-28 16:59:08 -0300

    Health Bar

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/HealthBarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    [SerializeField] private HealthController healthController;
+    [SerializeField] private Image healthBar;
+
+    private void Awake()
+    {
+        healthController.OnHealthChange.AddListener(OnHealthChangeHandler);
+    }
+
+    private void OnHealthChangeHandler()
+    {
+        healthBar.fillAmount = healthController.GetHealthPercentage();
+    }
+}
--- Farina_Gil_Nogara/Assets/_main/Scripts/HealthController.cs
+++ Farina_Gil_Nogara/Assets/_main/Scripts/HealthController.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class HealthController : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
+    [SerializeField] private float levelResetDelay = .25f;
+
+    public UnityEvent OnHealthChange = new UnityEvent();
 
     private int currentHealth;
 
@@ -16,6 +21,7 @@ public class HealthController : MonoBehaviour
     public void ResetLife()
     {
         currentHealth = maxHealth;
+        OnHealthChange.Invoke();
     }
 
     public void GetDamage(int damage)
@@ -27,9 +33,20 @@ public class HealthController : MonoBehaviour
             if (currentHealth <= 0)
             {
                 currentHealth = 0;
+                OnHealthChange.Invoke();
                 Kill();
+                LevelRestart();
+            }
+            else
+            {
+                OnHealthChange.Invoke();
             }
         }
+    }
+
+    public float GetHealthPercentage()
+    {
+        return (float) currentHealth / maxHealth;
     }
 
     public void GetHeal(int heal)
@@ -40,10 +57,22 @@ public class HealthController : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+        OnHealthChange.Invoke();
+
     }
 
     private void Kill()
     {
         Destroy(this.gameObject);
+    }
+
+    private void LevelRestart()
+    {
+        Invoke("ResetScene", levelResetDelay);
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
