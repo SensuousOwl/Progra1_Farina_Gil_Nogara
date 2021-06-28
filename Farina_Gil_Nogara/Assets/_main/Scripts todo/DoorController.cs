f6a24f1a93971d9d758187074d18ee4d752588e3
SensuousOwl <lfarina888@gmail.com>
2021-06-28 17:54:49 -0300

    Nada

--- Farina_Gil_Nogara/Assets/_main/Scripts todo/DoorController.cs
+++ Farina_Gil_Nogara/Assets/_main/Scripts todo/DoorController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    private List<int> doorKeyList = new List<int>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (doorKeyList.Count == 2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void AddToList()
+    {
+        doorKeyList.Add(1);
     }
 }
