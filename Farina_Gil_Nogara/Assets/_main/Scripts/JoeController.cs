3c0368fcd24c786b63b2030fdade6da5fcdac7e6
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-07-18 17:29:20 -0300

    Joe no camina

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/JoeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoeController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+
+
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+
+        if (myAnimator == null)
+        {
+            Debug.LogError("Missing Animator Component.");
+        }
+
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else
+        {
+            myAnimator.SetBool("IsWalking", false);
+        }
+    }
+}
+
---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/RobotController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class RobotController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float zRotation;
+    [SerializeField] private float boostForce = 5;
+
+    public UnityEvent OnDead = new UnityEvent();
+
+    private Animator myAnimator;
+    private Rigidbody2D myRigidbody;
+    private HealthController myHealthController;
+
+    private void Awake()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+
+        if (myAnimator == null)
+        {
+            Debug.LogError("Missing Animator Component.");
+        }
+
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+
+        myHealthController = gameObject.GetComponent<HealthController>();
+        myHealthController.OnDead.AddListener(OnDeadHandler);
+    }
+
+    private void OnDeadHandler()
+    {
+        OnDead.Invoke();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * boostForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            myAnimator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        else
+        {
+            myAnimator.SetBool("IsWalking", false);
+        }
+    }
+}
