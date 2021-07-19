632e1ff825c67c0d44b09222b9472c4949153149
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-07-19 18:55:22 -0300

    Space lvl pasa de nivel

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/SpaceWinTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceWinTrigger : MonoBehaviour
+{
+    [SerializeField] private SpaceLVLManager levelManager;
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            levelManager.Victory();
+        }
+    }*/
+
+    [SerializeField] private float timeRemaining = 10;
+    public bool timerIsRunning = false;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                levelManager.Victory();
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
