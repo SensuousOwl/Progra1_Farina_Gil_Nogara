df01067414baa62711ffd249cb410d6e3dc45f40
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-07-18 23:43:59 -0300

    World 2 y 3 casi terminados (Faltan cajas de dialogo + tienda)

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/World2Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class World2Controller : MonoBehaviour
+{
+    [SerializeField] private Button robotArenaButton;
+    [SerializeField] private Button acceptButton;
+
+
+    private void Awake()
+    {
+        robotArenaButton.onClick.AddListener(OnClickRobotArena);
+        acceptButton.onClick.AddListener(OnClickAccept);
+    }
+
+    private void OnClickRobotArena()
+    {
+        SceneManager.LoadScene("Robot Arena LVL 2");
+    }
+    private void OnClickAccept()
+    {
+        SceneManager.LoadScene("Box LVL 2");
+    }
+}
---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/World3Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class World3Controller : MonoBehaviour
+{
+    [SerializeField] private Button robotArenaButton;
+    [SerializeField] private Button acceptButton;
+
+
+    private void Awake()
+    {
+        robotArenaButton.onClick.AddListener(OnClickRobotArena);
+        acceptButton.onClick.AddListener(OnClickAccept);
+    }
+
+    private void OnClickRobotArena()
+    {
+        SceneManager.LoadScene("Robot Arena LVL 3");
+    }
+    private void OnClickAccept()
+    {
+        SceneManager.LoadScene("Box LVL 3");
+    }
+}
