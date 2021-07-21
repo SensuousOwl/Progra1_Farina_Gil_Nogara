4d8959719940b12b6ac2bc864d20711f23ec6681
Nicolas Gil Nogara <nicolasgn95@gmail.com>
2021-07-21 12:31:25 -0300

    Demo End Scene

---
+++ Farina_Gil_Nogara/Assets/_main/Scripts/DemoEndController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DemoEndController : MonoBehaviour
+{
+    [SerializeField] private Button mainMenuButton;
+
+    private void Awake()
+    {
+        mainMenuButton.onClick.AddListener(OnClickMainMenu);
+    }
+
+    private void OnClickMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
